aec3835131744fc3ae977d4651535deef042d127
HiveSite <office.hive.me@gmail.com>
2026-08-12 16:12:07 +0200

    Remove retired Studio jobs integrations

--- src/data/site.ts
+++ src/data/site.ts
@@ -28,14 +28,6 @@ export const site = {
       process.env.NEXT_PUBLIC_CONTACT_ENDPOINT,
       'https://script.google.com/macros/s/AKfycbyKu4_7m2qLOxnlfiDLYcwYbP4u1hswsp_mqH9rUj0ByyoUv1IVY0abc3gCK67QE4AQxQ/exec',
     ),
-    jobsEndpoint: publicEnv(
-      process.env.NEXT_PUBLIC_JOBS_ENDPOINT,
-      'https://script.google.com/macros/s/AKfycbwSwvILrfKoSaDYrOIegGY3E_4_eokard8yY9lurGfcEL7fQNsPxb5wT0ZRKwJfnC2_/exec',
-    ),
-    blogEndpoint: publicEnv(
-      process.env.NEXT_PUBLIC_BLOG_ENDPOINT,
-      'https://script.google.com/macros/s/AKfycbzGf2MSW8mS-sm9ZUnszl7rfYoP2WRJ-3reuYdeMz-4PD0adX2L5ZzOz47Xaa-w-45N/exec?sheet=Posts&onlyPublished=1',
-    ),
   },
   proof: [
     { value: '150+', label: 'ljudi u operativnom rosteru' },
