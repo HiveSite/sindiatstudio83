d78996cc3e4b2c67e38fe8d94cc764262d12d343
HiveSite <office.hive.me@gmail.com>
2026-08-12 15:33:43 +0200

    Fix crawl and noindex signaling

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,9 +5,10 @@ export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      { userAgent: '*', allow: '/', disallow: ['/hvala/'] },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
     sitemap: `${site.domain}/sitemap.xml`,
     host: site.domain,
   }
